00bfae3d1464e7c4dcbe5c4ab884ee49e95d6041
Sahand Raeisi <sahandraeisi@yahoo.com>
2018-12-26 14:07:19 +0330

    create project

---
+++ RX Swift Project/SecondViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SecondViewController.swift
+//  RX Swift Project
+//
+//  Created by Sahand Raeisi on 12/26/18.
+//  Copyright © 2018 Sahand Raeisi. All rights reserved.
+//
+
+import UIKit
+
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet fileprivate weak var imageView:UIImage!
+    @IBOutlet fileprivate weak var label:UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
+
---
+++ RX Swift Project/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  RX Swift Project
+//
+//  Created by Sahand Raeisi on 12/26/18.
+//  Copyright © 2018 Sahand Raeisi. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
